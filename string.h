d4d7976e0347038a96cb56711f4fd336bb9db96e
eszanna <131808772+eszanna@users.noreply.github.com>
2023-05-08 18:38:54 +0200

    Add files via upload

---
+++ string.h
@@ -0,0 +1,144 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <iostream>
+#include <cstring>
+class String {
+    char *pData;         /// pointer az adatra
+    size_t len;    /// hossz 0 nelkul
+public:
+
+size_t size() const { return len; }
+
+
+    /// Default konstruktor:
+    String() :pData(0), len(0) {}
+
+    /// C-stringet ad vissza
+    const char* c_str() const { if (pData) return pData; else return "";}
+
+    /// Konstruktor:(createStrFromChar)
+    String(char ch){
+        len = 1;
+        pData = new char[len+1];
+        pData[0] = ch;
+        pData[1] = '\0';
+}
+
+
+    /// Konstruktor:  (createStringFromCharStr)
+    String(const char *p){
+        len = strlen(p);
+        pData = new char[len+1];
+        strcpy(pData, p);
+}
+
+
+    String(const String& s1){
+        len = s1.len;
+        if ((pData = s1.pData) == NULL) return;
+        pData = new char[len+1];
+        strcpy(pData, s1.pData);
+}
+
+    /// Destruktor (disposeString)
+    ~String() { delete[] pData; }
+
+    void printDbg(const char *txt = "") const {
+        std::cout << txt << "[" << len << "], "
+                  << (pData ? pData : "(NULL)") << std::endl;
+    }
+
+  /// Operatorok:
+    String& operator=(const String& rhs_s) {
+        if (this != &rhs_s) {
+            delete[] pData;
+            len = rhs_s.len;
+            if ((pData = rhs_s.pData) == NULL) return *this;
+            pData = new char[len+1];
+            strcpy(pData, rhs_s.pData);
+        }
+        return *this;
+}
+
+    String& operator+=(const String& rhs_s) {
+        *this = *this + rhs_s;
+        return *this;
+    }
+
+    /// 2 Stringet concatString)
+
+    String operator+(const String& rhs_s) const {
+        String temp;
+        temp.len = len + rhs_s.len;
+        delete []temp.pData;
+            if (temp.len == 0) {
+                temp.pData = NULL;
+                return temp;
+            }
+        temp.pData = new char[temp.len+1];
+            if (pData == NULL)
+                temp.pData[0] = '\0';
+            else
+                strcpy(temp.pData, pData);
+            if (rhs_s.pData != NULL)
+                strcat(temp.pData, rhs_s.pData);
+
+        return temp;
+
+}
+
+    /// Sztrinhez karaktert (concatString)
+    String operator+(char rhs_c) const { return *this + String(rhs_c);}
+
+    /// A string egy megadott indexu elem ref-val ter vissza
+    char& operator[](unsigned int idx){
+        if (idx >= len) throw "String: indexelesi hiba";
+        return pData[idx];
+}
+
+    /// A string egy megadott indexu...
+
+    const char& operator[](unsigned int idx) const{
+        if (idx >= len) throw "String: indexelesi hiba";
+        return pData[idx];
+}
+    bool operator==(String& rhs){
+        return(this->pData == rhs.pData);
+
+}
+
+    void erase() { *this = ""; }
+};
+using std::cin;
+using std::ios_base;
+/// Globalis fgvek
+/// kii­r az ostream-re (printString)
+std::ostream& operator<<(std::ostream& os, const String& s0){
+    os << s0.c_str();
+    return os;
+}
+
+/// Beolvas az istreamrol egy szot egy string-be.
+std::istream& operator>>(std::istream& is, String& s0){
+    unsigned char ch;
+    s0 = String("");
+	std::ios_base::fmtflags fl = is.flags();
+	is.setf(ios_base::skipws);
+    while (is >> ch) {
+	    is.unsetf(ios_base::skipws);
+        if (isspace(ch)) {
+            is.putback(ch);
+            break;
+        } else {
+            s0 = s0 + ch;
+        }
+    }
+	is.setf(fl);
+    return is;
+}
+
+/// String operator+(char ch, const String& str);
+inline String operator+(char ch, const String& str) { return String(ch) + str; }
+
+#endif
