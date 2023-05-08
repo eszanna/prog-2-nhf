92721cb5fba1f7fc14b98626b092f15c50161429
eszanna <131808772+eszanna@users.noreply.github.com>
2023-05-08 18:38:23 +0200

    Add files via upload

--- filmtar.hpp
+++ filmtar.hpp
@@ -5,76 +5,74 @@
 #include <string>
 #include <iostream>
 
-
-//#define MEMTRACE
-//#define MEMTRACE_CPP
 #include "memtrace.h"
-
-
 #include "film.h"
 
 
 ///tobbfele filmet tarolni tudo filmtar osztaly
-template<class T>
 
-class Filmtar : public Film{
+
+class Filmtar : public Film {
     static const size_t kezdo_kapacitas = 10;
-    T* tar;
+    Film** tar;
     size_t kapacitas;
     size_t db;
 
 public:
     ///konstruktor default ertekekkel a memszivargas elkerulese erdekeben 0-val es nullptr-rel kezdve
-    Filmtar() : tar(new T[kezdo_kapacitas]()), kapacitas(kezdo_kapacitas), db(0) { }
+    Filmtar() : tar(new Film*[kezdo_kapacitas]()), kapacitas(kezdo_kapacitas), db(0) { }
 
     ///visszadja, hogy hany film van a tarban
     size_t size() const { return db; }
 
     ///parameterkent kapott filmet tesz a tarba
-    void felvesz(const T& ap){
-        if(db >= kapacitas){
-            //std::cout << "Ujat kellett foglalni mert tele lett" << std::endl; //ha tele az eddigi, ujat foglalunk, 10-zel nagyobbat
+    void felvesz(const Film& ap){
+        if (db >= kapacitas) {
+            //ha tele az eddigi, ujat foglalunk, 10-zel nagyobbat
             kapacitas += 10;
-            T* uj_tar = new T[kapacitas]();
-            for(size_t i = 0; i<db; i++)          //belemasoljuk a regit
+            Film** uj_tar = new Film* [kapacitas]();
+            for (size_t i = 0; i < db; i++) {         //belemasoljuk a regit
                 uj_tar[i] = tar[i];
+                delete tar[i];
+            }
 
             delete[] tar;
             tar = uj_tar;
+            for (size_t i = 0; i < db; ++i)
+                delete uj_tar[i];
         }
-        tar[db++] = ap;                             //mindenkeppen hozzaadjuk a vegere
+        tar[db++] = new Film(ap); //mindenkeppen hozzaadjuk a vegere
     }
 
     ///kiirja az osszes filmet
     void kiir() const {
         for (size_t i = 0; i < db; i++)
-            tar[i].kiir();
+            tar[i]->kiir();
     }
 
     ///torli a parameterkent kapott filmet
     void torol(size_t idx){
         if(idx>db)
             throw "Index error";
-        tar[idx] = T();
+        delete tar[idx];
         for (size_t i = idx; i < db-1; i++) {
             tar[i] = tar[i+1];
-            
+
         }
         db--;
     }
 
-    ///destruktor
-   // ~Filmtar(){
-    // for(size_t i = 0; i < db; i++)
-    //    torol(i);
-    //}
-
-    ~Filmtar() { delete[] tar; }
+    ~Filmtar() {
+         for (size_t i = 0; i < db+1; i++) {
+            delete tar[i];
+    }
+    delete[] tar;
+    }
 
     ///indexelo operator
-    Film operator[](size_t idx) {
+    Film& operator[](size_t idx) {
         if(idx < db)
-            return tar[idx];
+            return *tar[idx];
         else
             throw "Index error";
     }
